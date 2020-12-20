ff7a8bf57403c02031096270a6b34309634180b2
S. V. Paulauskas <stanpaulauskas@gmail.com>
2020-12-20 11:35:53 -0500

    Adding some work related to generating probability distributions from histograms

---
+++ python/probability_distribution_from_data.py
@@ -0,0 +1,19 @@
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from numpy import random
+
+from sample_spectrum import cs137_spectrum
+
+root_binning_factor = 2
+
+choices = [x[0] / root_binning_factor for x in cs137_spectrum]
+counts = [x[1] for x in cs137_spectrum]
+weights = [x / sum(counts) for x in counts]
+
+num_choices = 2563370
+bins = 800
+
+ax = plt.gca()
+DataFrame(random.choice(choices, num_choices, p=weights)).hist(bins=bins * root_binning_factor,
+                                                               range=[0, bins], ax=ax).flatten()[
+    0].get_figure().show()
